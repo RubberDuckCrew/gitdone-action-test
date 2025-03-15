72b994b0445f70093781ba5d8a6df2d2484fe324
Friedinger <info@friedinger.org>
2025-03-15 22:46:55 +0100

    :sparkles: Add octocat request

--- lib/views/HomeView.dart
+++ lib/views/HomeView.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gitdone/widgets/Appbar.dart';
+import 'package:gitdone/utility/GithubApiHandler.dart';
+import 'package:gitdone/utility/TokenHandler.dart';
 
 class Homeview extends StatefulWidget {
   const Homeview({super.key});
@@ -9,12 +11,36 @@ class Homeview extends StatefulWidget {
 }
 
 class _HomeviewState extends State<Homeview> {
+  String _response = "Loading...";
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    TokenHandler tokenHandler = TokenHandler();
+    String? token = await tokenHandler.getToken();
+    if (token != null) {
+      GithubApiHandler apiHandler = GithubApiHandler(token);
+      final response = await apiHandler.get('/octocat');
+      setState(() {
+        _response = response.body;
+      });
+    } else {
+      setState(() {
+        _response = "Token not found";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: NormalAppBar(),
       body: Center(
-        child: Text("Welcome to GitDone"),
+        child: Text(_response),
       ),
     );
   }
