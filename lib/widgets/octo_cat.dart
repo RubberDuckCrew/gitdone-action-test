cd8eb581bd0ed25d39f8889a141b527b6a9a71ba
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-03-25 21:54:03 +0100

    🚏 Introduce OctoCatProvider
    - Uses a provider to provide the octocat to the OctoCat widget.
    - Add refresh logic to the OctoCatProvider.
    - Remove fetch logic from the widget to provider.
    - Allow fetching a new octocat via the provider.
    - Provide `futureResponse` as the data source.

--- lib/widgets/octo_cat.dart
+++ lib/widgets/octo_cat.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:provider/provider.dart';
 
 import '../utility/github_api_handler.dart';
 import '../utility/token_handler.dart';
@@ -14,46 +15,72 @@ class OctoCat extends StatefulWidget {
 class _OctoCatState extends State<OctoCat> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        FutureBuilder(
-            future: _fetchData(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              }
-              if (snapshot.hasData) {
-                return SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Text.rich(
-                      TextSpan(
-                          text: snapshot.data!.body.toString(),
-                          style: TextStyle(
-                              fontSize: 8,
-                              letterSpacing: 1,
-                              fontFamily: "Courier")),
-                      textAlign: TextAlign.left,
-                    ));
-              }
-              return const Center(child: Text("Could not fetch octocat"));
-            }),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {});
-          },
-          child: Text("Fetch new octocat"),
-        )
-      ],
-    );
+    return ChangeNotifierProvider(
+        create: (_) => OctoCatProvider(),
+        child: Consumer<OctoCatProvider>(builder: (context, provider, child) {
+          return Column(
+            children: [
+              FutureBuilder(
+                  future: provider.futureResponse,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasData) {
+                      return SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Text.rich(
+                            TextSpan(
+                                text: snapshot.data!.body.toString(),
+                                style: TextStyle(
+                                    fontSize: 8,
+                                    letterSpacing: 1,
+                                    fontFamily: "Courier")),
+                            textAlign: TextAlign.left,
+                          ));
+                    }
+                    return const Center(child: Text("Could not fetch octocat"));
+                  }),
+              ElevatedButton(
+                onPressed: () {
+                  provider.refreshData();
+                },
+                child: Text("Fetch new octocat"),
+              )
+            ],
+          );
+        }));
+  }
+}
+
+class OctoCatProvider extends ChangeNotifier {
+  OctoCatProvider() {
+    fetchData();
   }
 
-  Future<Response> _fetchData() async {
-    TokenHandler tokenHandler = TokenHandler();
-    String? token = await tokenHandler.getToken();
-    if (token != null) {
-      GithubApiHandler apiHandler = GithubApiHandler(token);
-      return await apiHandler.get('/octocat');
+  Future<Response>? _futureResponse;
+
+  Future<Response>? get futureResponse => _futureResponse;
+
+  Future<Response> fetchData() async {
+    if (_futureResponse == null) {
+      TokenHandler tokenHandler = TokenHandler();
+      String? token = await tokenHandler.getToken();
+      if (token != null) {
+        GithubApiHandler apiHandler = GithubApiHandler(token);
+        _futureResponse = apiHandler.get('/octocat');
+        notifyListeners();
+      } else {
+        _futureResponse = Future.value(Response('No token', 401));
+      }
     }
-    return Response('No token', 401);
+    return _futureResponse!;
+  }
+
+  void refreshData() {
+    _futureResponse = null;
+    //TODO: Does this need to be called?
+    //notifyListeners();
+    fetchData();
   }
 }
