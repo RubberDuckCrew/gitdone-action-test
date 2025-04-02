8ecd2ed39d4f1fc063570c4f99c64f94c76daf47
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-04-02 21:13:03 +0200

    :recycle: Add mocktail dependency and implement unit tests for TokenHandler

--- lib/utility/token_handler.dart
+++ lib/utility/token_handler.dart
@@ -1,18 +1,21 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class TokenHandler {
-  final FlutterSecureStorage storage = FlutterSecureStorage();
+  FlutterSecureStorage storage = FlutterSecureStorage();
   TokenHandler();
 
   Future<void> saveToken(String token) async {
     await storage.write(key: 'auth_token', value: token);
   }
+
   Future<String?> getToken() async {
     return await storage.read(key: 'auth_token');
   }
+
   Future<void> deleteToken() async {
     await storage.delete(key: 'auth_token');
   }
+
   Future<bool> hasToken() async {
     String? token = await storage.read(key: 'auth_token');
     return token != null;
