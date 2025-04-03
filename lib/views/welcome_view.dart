ac79dc084c34a994af1beb560fd236a051e8b32a
Friedinger <info@friedinger.org>
2025-04-03 13:47:35 +0200

    :lipstick: Style and improve welcome page

---
+++ lib/views/welcome_view.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gitdone/views/login_github_view.dart';
+import 'package:gitdone/views/login_token_view.dart';
+import 'package:gitdone/widgets/app_title.dart';
+
+class WelcomeView extends StatefulWidget {
+  const WelcomeView({super.key});
+
+  @override
+  State<WelcomeView> createState() => _WelcomeViewState();
+}
+
+class _WelcomeViewState extends State<WelcomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: [
+          const Spacer(flex: 1),
+          Expanded(
+              flex: 2,
+              child: SvgPicture.asset(
+                "assets/icons/app/gitdone.svg",
+              )),
+          const AppTitleWidget(fontSize: 45),
+          const Text("Store your todos in GitHub issues",
+              style: TextStyle(fontSize: 20)),
+          const Spacer(flex: 1),
+          const Text("To use GitDone, please login with your GitHub account",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          const Padding(padding: EdgeInsets.all(8.0)),
+          FilledButton(
+              onPressed: goToLoginGithubView,
+              child: const Text("Login with GitHub")),
+          const Text("or"),
+          FilledButton(
+              onPressed: goToLoginTokenView,
+              child: const Text("Enter Personal Access Token")),
+          const Spacer(flex: 1),
+        ],
+      ),
+    ));
+  }
+
+  void goToLoginGithubView() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginGithubView()),
+    );
+  }
+
+  void goToLoginTokenView() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginTokenView()),
+    );
+  }
+
+  // void openLoginBottomSheet() {
+  //   showModalBottomSheet(
+  //     showDragHandle: true,
+  //     isScrollControlled: true,
+  //     scrollControlDisabledMaxHeightRatio: 0.5,
+  //     context: context,
+  //     builder: (context) {
+  //       return SizedBox(
+  //           height: MediaQuery.of(context).size.height * 0.75,
+  //           child: Column(
+  //             children: [
+  //               Expanded(child: TitleWidget()),
+  //               Expanded(
+  //                 flex: 2,
+  //                 child: GithubTokenInput(),
+  //               ),
+  //               Spacer(
+  //                 flex: 2,
+  //               )
+  //             ],
+  //           ));
+  //     },
+  //   );
+  // }
+}
