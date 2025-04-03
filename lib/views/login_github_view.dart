597bcf42f82621254b8f11df7049d33273aad44f
Friedinger <info@friedinger.org>
2025-04-03 15:26:43 +0200

    :lipstick: Improve GitHub login view with instructions and dummy code display

--- lib/views/login_github_view.dart
+++ lib/views/login_github_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gitdone/widgets/github_login_button.dart';
 
 import '../widgets/app_bar.dart';
+import '../widgets/page_title.dart';
 
 class LoginGithubView extends StatefulWidget {
   const LoginGithubView({super.key});
@@ -15,10 +16,32 @@ class _LoginGithubViewState extends State<LoginGithubView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: NormalAppBar(),
-      body: Center(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [GithubLoginButton()],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            PageTitleWidget(
+              title: "GitHub OAuth Login",
+            ),
+            RichText(
+                text: TextSpan(
+              style: TextStyle(fontSize: 18),
+              text:
+                  "To log in with GitHub, please copy the code below and open the browser. There authenticate with your GitHub account, paste the device code and authorize the app. After that, close the browser.",
+            )),
+            Text("Please enter this code in the browser: "),
+            SelectableText(
+              "123456",
+              style: TextStyle(fontSize: 20),
+            ),
+            FilledButton(
+              onPressed: () {},
+              child: Text("Copy code and open browser"),
+            ),
+            Text("Temporary fallback:"),
+            GithubLoginButton()
+          ],
         ),
       ),
     );
