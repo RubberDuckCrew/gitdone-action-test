ca6dd1f79a7fc6da4bd365b8e1c143ee9279a770
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-04-16 12:30:07 +0200

    :technologist: Refactor LoginGithubViewModel to improve state management and remove context dependency

--- lib/ui/view_models/login_github_view_model.dart
+++ lib/ui/view_models/login_github_view_model.dart
@@ -1,36 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:gitdone/ui/views/home_view.dart';
 import 'package:gitdone/utility/github_oauth_handler.dart';
 
 class LoginGithubViewModel extends ChangeNotifier {
-  final GitHubAuth githubAuth;
-  final BuildContext context;
+  final GitHubAuth _githubAuth;
+
+  final ValueNotifier<bool> showProgressIndicatorNotifier =
+      ValueNotifier(false);
+  final ValueNotifier<bool> fetchedUserCode = ValueNotifier(false);
+
+  /// Callback function to show the user an informational message.
+  Function(String) infoCallback;
 
   /// Creates an instance of [LoginGithubViewModel].
-  LoginGithubViewModel({required this.context})
-      : githubAuth = GitHubAuth(callbackFunction: (text) {
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text(text), duration: Duration(seconds: 2)));
-        });
-  bool get inLoginProcess => githubAuth.inLoginProcess;
+  LoginGithubViewModel({required this.infoCallback})
+      : _githubAuth = GitHubAuth(callbackFunction: infoCallback);
+
+  bool get inLoginProcess => _githubAuth.inLoginProcess;
 
   /// Starts the login process and returns the user code.
   ///
   /// Returns the `userCode` to be displayed to the user.
   Future<String?> startLogin() async {
-    return await githubAuth.startLoginProcess();
+    String? userCode = await _githubAuth.startLoginProcess();
+    if (userCode.isNotEmpty) fetchedUserCode.value = true;
+    return userCode;
   }
 
   /// Launches the browser and copies the user code to the clipboard.
-  void launchBrowser(String userCode) async {
-    Clipboard.setData(ClipboardData(text: userCode));
-    githubAuth.launchBrowser();
-
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (_) => Center(child: CircularProgressIndicator()),
-    );
+  void launchBrowser() async {
+    showProgressIndicatorNotifier.value = true;
+    Clipboard.setData(ClipboardData(text: _githubAuth.userCode));
+    _githubAuth.launchBrowser();
   }
 
   /// Continues the login process by polling for the token and executing the
@@ -38,8 +40,11 @@ class LoginGithubViewModel extends ChangeNotifier {
   Future<void> continueLogin(
       {required VoidCallback onSuccess,
       required VoidCallback onFailure}) async {
-    final authenticated = await githubAuth.pollForToken();
-    if (context.mounted) Navigator.of(context).maybePop();
+    final authenticated = await _githubAuth.pollForToken();
+
+    // Reactively notify the login process state
+    showProgressIndicatorNotifier.value = false;
+
     if (authenticated) {
       onSuccess();
     } else {
@@ -47,8 +52,31 @@ class LoginGithubViewModel extends ChangeNotifier {
     }
   }
 
+  /// Handles Lifecycle events from the UI
+  void handleAppLifecycleState(AppLifecycleState state, BuildContext context) {
+    if (state == AppLifecycleState.resumed && inLoginProcess) {
+      continueLogin(
+        onSuccess: () {
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => Homeview()),
+            (Route route) => false,
+          );
+        },
+        onFailure: () {
+          if (Navigator.canPop(context)) {
+            Navigator.of(context).pop();
+          }
+          infoCallback("Login failed. Please try again.");
+        },
+      );
+    }
+  }
+
   /// Resets the GitHub authentication handler.
   void disposeHandler() {
-    githubAuth.resetHandler();
+    _githubAuth.resetHandler();
   }
+
+  bool get authenticated => _githubAuth.isAuthenticated;
+  String get userCode => _githubAuth.userCode;
 }
