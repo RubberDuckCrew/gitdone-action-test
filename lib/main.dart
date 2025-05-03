b019f4e6ab5933ba1ef6d61588ce6cf9d391fd56
Friedinger <info@friedinger.org>
2025-05-03 13:27:54 +0200

    :technologist: Add prod and dev flavors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gitdone/app.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const App());
+  const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'dev');
+  runApp(
+    Provider<String>.value(
+      value:
+          flavor, // Get flavor anywhere with final flavor = Provider.of<String>(context);
+      child: const App(),
+    ),
+  );
 }
