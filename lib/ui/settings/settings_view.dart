f85ece4149af013717ebe4e74f4522d6c570d2e5
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-06 08:45:45 +0200

    :recycle: Rename RepositorySelector.dart to repository_selector.dart

--- lib/ui/settings/settings_view.dart
+++ lib/ui/settings/settings_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gitdone/ui/settings/widgets/RepositorySelector.dart';
+import 'package:gitdone/ui/settings/widgets/repository_selector.dart';
 
 class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
