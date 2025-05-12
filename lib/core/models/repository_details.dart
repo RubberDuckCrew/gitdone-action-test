f839069f23f76ebc18e752ab518e120881600bc7
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-12 13:27:17 +0200

    :sparkles: Add RepositoryDetails class for handling local repository information

---
+++ lib/core/models/repository_details.dart
@@ -0,0 +1,29 @@
+import 'package:github_flutter/github.dart';
+
+class RepositoryDetails {
+  final String name;
+  final String owner;
+  final String avatarUrl;
+
+  RepositoryDetails({
+    required this.name,
+    required this.owner,
+    required this.avatarUrl,
+  });
+
+  factory RepositoryDetails.fromRepository(Repository repo) {
+    return RepositoryDetails(
+      name: repo.name,
+      owner: repo.owner!.login,
+      avatarUrl: repo.owner!.avatarUrl,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'owner': owner,
+      'avatarUrl': avatarUrl,
+    };
+  }
+}
