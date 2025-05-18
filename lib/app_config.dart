ea9d48a97f13e98f4b54f63aca3de91e11e4953e
Friedinger <info@friedinger.org>
2025-05-18 17:31:26 +0200

    :memo: Comment app config

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -5,9 +5,11 @@ class AppConfig {
 
   AppConfig._internal();
 
+  /// The flavor of the app. This is set by the build system.
   static final String flavor =
       const String.fromEnvironment("FLAVOR", defaultValue: "development");
 
+  /// The commit hash on which the app was built. This is set by the build system.
   static final String gitCommit =
       const String.fromEnvironment("GIT_COMMIT", defaultValue: "uncommitted")
           .substring(0, 7);
