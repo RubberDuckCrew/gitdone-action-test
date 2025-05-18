468bf60c52a4675981cfc07286aa06542ac49ba8
Friedinger <info@friedinger.org>
2025-05-18 17:57:48 +0200

    :sparkles: Add app version and build number to app config

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,3 +1,5 @@
+import 'package:package_info_plus/package_info_plus.dart';
+
 class AppConfig {
   static final AppConfig _instance = AppConfig._internal();
 
@@ -10,7 +12,17 @@ class AppConfig {
       const String.fromEnvironment("FLAVOR", defaultValue: "development");
 
   /// The commit hash on which the app was built. This is set by the build system.
-  static final String gitCommit =
-      const String.fromEnvironment("GIT_COMMIT", defaultValue: "uncommitted")
-          .substring(0, 7);
+  static final String gitCommit = const bool.hasEnvironment("GIT_COMMIT")
+      ? const String.fromEnvironment("GIT_COMMIT").substring(0, 7)
+      : "uncommitted";
+
+  /// App version and build number, populated in [init].
+  static late final String version;
+  static late final String buildNumber;
+
+  static Future<void> init() async {
+    final info = await PackageInfo.fromPlatform();
+    version = info.version;
+    buildNumber = info.buildNumber;
+  }
 }
