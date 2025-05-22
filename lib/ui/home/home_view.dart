84988a5c975a5d3f8735b6652ad360da117e0807
Markus Neumann <markus.neumann0@hm.edu>
2025-05-22 18:46:14 +0200

    :sparkles: Add search field and Todo widget to Homeview

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gitdone/core/models/token_handler.dart';
 import 'package:gitdone/ui/_widgets/octo_cat.dart';
+import 'package:gitdone/ui/_widgets/todo.dart';
 import 'package:gitdone/ui/welcome/welcome_view.dart';
 
 class Homeview extends StatefulWidget {
@@ -28,14 +29,27 @@ class _HomeviewState extends State<Homeview> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          OctoCat(),
-          ElevatedButton(onPressed: logout, child: const Text("Logout")),
-        ],
-      ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Search',
+              prefixIcon: const Icon(Icons.search),
+            ),
+          ),
+        ),
+        Text("Filled Buttons here"),
+        Todo(
+          title: "Example Todo Item",
+          description:
+              "This is a description of the todo item. It can be a bit longer to show how it looks.",
+        ),
+        OctoCat(),
+        ElevatedButton(onPressed: logout, child: const Text("Logout")),
+      ],
     );
   }
 }
