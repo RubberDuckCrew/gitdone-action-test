a2f3987ca537f508bd3bdbdc4544196bc7e39637
Markus Neumann <markus.neumann0@hm.edu>
2025-05-22 19:28:53 +0200

    :sparkles: Update Todo display to show a scrollable list of multiple items

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gitdone/core/models/token_handler.dart';
-import 'package:gitdone/ui/_widgets/octo_cat.dart';
 import 'package:gitdone/ui/_widgets/todo.dart';
 import 'package:gitdone/ui/welcome/welcome_view.dart';
 
@@ -42,13 +41,31 @@ class _HomeviewState extends State<Homeview> {
           ),
         ),
         Text("Filled Buttons here"),
-        Todo(
-          title: "Example Todo Item",
-          description:
-              "This is a description of the todo item. It can be a bit longer to show how it looks.",
+        Expanded(
+          child: ListView(
+            shrinkWrap: true,
+            physics: AlwaysScrollableScrollPhysics(),
+            children: [
+              Todo(title: "Todo 1", description: "Description of Todo 1"),
+              Todo(title: "Todo 2", description: "Description of Todo 2"),
+              Todo(title: "Todo 3", description: "Description of Todo 3"),
+              Todo(title: "Todo 4", description: "Description of Todo 4"),
+              Todo(title: "Todo 5", description: "Description of Todo 5"),
+              Todo(title: "Todo 6", description: "Description of Todo 6"),
+              Todo(title: "Todo 7", description: "Description of Todo 7"),
+              Todo(title: "Todo 8", description: "Description of Todo 8"),
+              Todo(title: "Todo 9", description: "Description of Todo 9"),
+              Todo(title: "Todo 10", description: "Description of Todo 10"),
+              Todo(title: "Todo 11", description: "Description of Todo 11"),
+              Todo(title: "Todo 12", description: "Description of Todo 12"),
+              Todo(title: "Todo 13", description: "Description of Todo 13"),
+              Todo(title: "Todo 14", description: "Description of Todo 14"),
+              Todo(title: "Todo 15", description: "Description of Todo 15"),
+            ],
+          ),
         ),
-        OctoCat(),
-        ElevatedButton(onPressed: logout, child: const Text("Logout")),
+        //OctoCat(),
+        //ElevatedButton(onPressed: logout, child: const Text("Logout")),
       ],
     );
   }
