461380d9729c6423c5a55af754833a826f1cea76
Markus Neumann <markus.neumann0@hm.edu>
2025-05-22 19:48:31 +0200

    :sparkles: Add floating action button

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -28,47 +28,60 @@ class _HomeviewState extends State<Homeview> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Search',
-              prefixIcon: const Icon(Icons.search),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 16),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Search',
+                  prefixIcon: const Icon(Icons.search),
+                ),
+              ),
             ),
-          ),
+            Text("Filled Buttons here"),
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                physics: AlwaysScrollableScrollPhysics(),
+                children: [
+                  Todo(title: "Todo 1", description: "Description of Todo 1"),
+                  Todo(title: "Todo 2", description: "Description of Todo 2"),
+                  Todo(title: "Todo 3", description: "Description of Todo 3"),
+                  Todo(title: "Todo 4", description: "Description of Todo 4"),
+                  Todo(title: "Todo 5", description: "Description of Todo 5"),
+                  Todo(title: "Todo 6", description: "Description of Todo 6"),
+                  Todo(title: "Todo 7", description: "Description of Todo 7"),
+                  Todo(title: "Todo 8", description: "Description of Todo 8"),
+                  Todo(title: "Todo 9", description: "Description of Todo 9"),
+                  Todo(title: "Todo 10", description: "Description of Todo 10"),
+                  Todo(title: "Todo 11", description: "Description of Todo 11"),
+                  Todo(title: "Todo 12", description: "Description of Todo 12"),
+                  Todo(title: "Todo 13", description: "Description of Todo 13"),
+                  Todo(title: "Todo 14", description: "Description of Todo 14"),
+                  Todo(title: "Todo 15", description: "Description of Todo 15"),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: logout,
+                child: const Text("Logout"),
+              ),
+            ),
+          ],
         ),
-        Text("Filled Buttons here"),
-        Expanded(
-          child: ListView(
-            shrinkWrap: true,
-            physics: AlwaysScrollableScrollPhysics(),
-            children: [
-              Todo(title: "Todo 1", description: "Description of Todo 1"),
-              Todo(title: "Todo 2", description: "Description of Todo 2"),
-              Todo(title: "Todo 3", description: "Description of Todo 3"),
-              Todo(title: "Todo 4", description: "Description of Todo 4"),
-              Todo(title: "Todo 5", description: "Description of Todo 5"),
-              Todo(title: "Todo 6", description: "Description of Todo 6"),
-              Todo(title: "Todo 7", description: "Description of Todo 7"),
-              Todo(title: "Todo 8", description: "Description of Todo 8"),
-              Todo(title: "Todo 9", description: "Description of Todo 9"),
-              Todo(title: "Todo 10", description: "Description of Todo 10"),
-              Todo(title: "Todo 11", description: "Description of Todo 11"),
-              Todo(title: "Todo 12", description: "Description of Todo 12"),
-              Todo(title: "Todo 13", description: "Description of Todo 13"),
-              Todo(title: "Todo 14", description: "Description of Todo 14"),
-              Todo(title: "Todo 15", description: "Description of Todo 15"),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(onPressed: logout, child: const Text("Logout")),
-        ),
-      ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
