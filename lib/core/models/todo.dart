4fc0b15f6519c66285c7a14d1e8cd1b39bd79577
Markus Neumann <markus.neumann0@hm.edu>
2025-05-25 14:44:52 +0200

    :sparkles:  Enhance Todo model with additional fields for better tracking and management

--- lib/core/models/todo.dart
+++ lib/core/models/todo.dart
@@ -1,17 +1,35 @@
 import 'package:github_flutter/github.dart';
 
 class Todo {
-  final String title;
-  final String description;
+  String title;
+  String description;
+  List<IssueLabel> labels = <IssueLabel>[];
+  DateTime createdAt;
+  DateTime? updatedAt;
+  DateTime? closedAt;
 
-  Todo({required this.title, required this.description});
+  Todo({
+    required this.title,
+    required this.description,
+    required this.createdAt,
+    required this.labels,
+    this.updatedAt,
+    this.closedAt,
+  });
 
   @override
   String toString() {
-    return 'Todo{title: $title, description: $description}';
+    return 'Todo(title: $title, description: $description, createdAt: $createdAt, updatedAt: $updatedAt, closedAt: $closedAt, labels: $labels)';
   }
 
   static Todo fromIssue(Issue issue) {
-    return Todo(title: issue.title, description: issue.body);
+    return Todo(
+      title: issue.title,
+      description: issue.body,
+      createdAt: issue.createdAt!,
+      updatedAt: issue.updatedAt,
+      closedAt: issue.closedAt,
+      labels: issue.labels,
+    );
   }
 }
