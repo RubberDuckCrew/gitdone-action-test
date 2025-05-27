7db8f2da991d68c9ed81cd4b36b6931dab929f06
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-27 16:01:38 +0200

    :bulb: Add documentation comments for TokenHandler class and its methods

--- lib/core/models/token_handler.dart
+++ lib/core/models/token_handler.dart
@@ -2,33 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:gitdone/ui/welcome/welcome_view.dart';
 
+/// A class to handle token storage and retrieval using Flutter Secure Storage.
 class TokenHandler {
-  FlutterSecureStorage storage = FlutterSecureStorage();
+  /// Singleton instance of [TokenHandler].
   TokenHandler();
+  // TODO(everyone): @VisibleForTesting
+  FlutterSecureStorage storage = const FlutterSecureStorage();
 
-  Future<void> saveToken(String token) async {
-    await storage.write(key: 'auth_token', value: token);
+  /// Saves the provided token to secure storage.
+  Future<void> saveToken(final String token) async {
+    await storage.write(key: "auth_token", value: token);
   }
 
-  Future<String?> getToken() async {
-    return await storage.read(key: 'auth_token');
-  }
+  /// Retrieves the token from secure storage.
+  Future<String?> getToken() => storage.read(key: "auth_token");
 
+  /// Deletes the token from secure storage.
   Future<void> deleteToken() async {
-    await storage.delete(key: 'auth_token');
+    await storage.delete(key: "auth_token");
   }
 
+  /// Checks if a token exists in secure storage.
   Future<bool> hasToken() async {
-    String? token = await storage.read(key: 'auth_token');
+    final String? token = await storage.read(key: "auth_token");
     return token != null;
   }
 
-  static void logout(context) async {
-    TokenHandler tokenHandler = TokenHandler();
+  /// Logs out the user by deleting the token and navigating to the welcome view
+  static Future<void> logout(final BuildContext context) async {
+    final tokenHandler = TokenHandler();
     await tokenHandler.deleteToken();
-    Navigator.pushReplacement(
+    // TODO(IamPekka058): Guard Check
+    await Navigator.pushReplacement(
       context,
-      MaterialPageRoute(builder: (context) => const WelcomeView()),
+      MaterialPageRoute(builder: (final context) => const WelcomeView()),
     );
   }
 }
