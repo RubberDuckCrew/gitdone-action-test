e444e09409c0681cccf79279f933cdaac7c1f542
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-27 15:55:40 +0200

    :bulb: Add documentation comments for NormalAppBar widget

--- lib/ui/_widgets/app_bar.dart
+++ lib/ui/_widgets/app_bar.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:gitdone/core/theme/app_color.dart';
 import 'package:gitdone/ui/_widgets/app_title.dart';
 
+/// A normal app bar that is used in most of the views.
 class NormalAppBar extends StatelessWidget implements PreferredSizeWidget {
+  /// Creates a normal app bar with an optional back button.
+  const NormalAppBar({super.key, this.backVisible = true});
+
+  /// Whether the back button should be visible.
   final dynamic backVisible;
 
   const NormalAppBar({super.key, this.backVisible = true});
