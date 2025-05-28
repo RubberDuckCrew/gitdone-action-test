f3d66d70f47be3432134af5435ee4df3e13aeefa
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-05-28 19:34:56 +0200

    :rotating_light: Fix home_screen_view_model.dart linting

--- lib/ui/home/home_screen_view_model.dart
+++ lib/ui/home/home_screen_view_model.dart
@@ -1,9 +1,13 @@
 import "package:flutter/widgets.dart";
 
+/// The view model for the home screen, which manages the selected index
 class HomeScreenViewModel extends ChangeNotifier {
   int _selectedIndex = 0;
+
+  /// The currently selected index of the bottom navigation bar.
   int get selectedIndex => _selectedIndex;
 
+  /// Updates the selected index of the bottom navigation bar.
   void updateIndex(final int index) {
     _selectedIndex = index;
     notifyListeners();
