974d8ffcbcce77c7a326f0bacde8923f319444ea
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-05-28 19:36:44 +0200

    :rotating_light: Fix text_link.dart linting

--- lib/ui/_widgets/text_link.dart
+++ lib/ui/_widgets/text_link.dart
@@ -3,18 +3,23 @@ import "package:flutter/material.dart";
 import "package:gitdone/core/theme/app_color.dart";
 import "package:url_launcher/url_launcher.dart";
 
+/// A widget that displays a clickable text link.
 class TextLinkWidget {
-  TextLinkWidget({required this.text, required this.url});
-  final String text;
-  final String url;
+  /// Creates an instance of [TextLinkWidget] with the given [text] and [url].
+  TextLinkWidget({required final String text, required final String url})
+    : _url = url,
+      _text = text;
+  final String _text;
+  final String _url;
 
+  /// Converts the [TextLinkWidget] to a [TextSpan] with a tap gesture recognizer.
   TextSpan toTextSpan() => TextSpan(
-    text: text,
+    text: _text,
     style: TextStyle(color: AppColor.colorScheme.secondary),
     recognizer: TapGestureRecognizer()
       ..onTap = () async {
-        if (!await launchUrl(Uri.parse(url))) {
-          throw "Could not launch $url";
+        if (!await launchUrl(Uri.parse(_url))) {
+          throw Exception("Could not launch $_url");
         }
       },
   );
